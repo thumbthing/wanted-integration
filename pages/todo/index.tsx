bcfe812a7eab53593ff36b994295cdb4a6a0aa91
thumbthing <ilscc0902@gmail.com>
2023-09-21 03:55:48 +0900

    Feat: page에 Todo Layout 적용

--- pages/todo/index.tsx
+++ pages/todo/index.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import SignForm from '@/components/todo/sign/SignForm';
+import TodoLayout from './Todo.Layout';
+
 export default function TodoPage() {
-  return <div>TodoPage</div>;
+  return (
+    <TodoLayout>
+      <div>
+        <SignForm />
+      </div>
+    </TodoLayout>
+  );
 }
