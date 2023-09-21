b12d20f8ec3fdff55c0355149a269d45feae101c
thumbthing <ilscc0902@gmail.com>
2023-09-21 16:57:38 +0900

    Feat: 이메일, 패스워드 검증 util 함수 생성

---
+++ util/todo/checkInputValid.ts
@@ -0,0 +1,11 @@
+export const checkEmailValid = (input: string) => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const isValid = emailRegex.test(input);
+  return isValid;
+};
+
+export const checkPasswordValid = (input: string) => {
+  const passwordLength = 8;
+  const isValid = input.length > passwordLength;
+  return isValid;
+};
