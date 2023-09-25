d398f3e4856c776bd16963753b1ca651aedbf898
thumbthing <ilscc0902@gmail.com>
2023-09-26 03:20:13 +0900

    Feat: 로그인, 회원가입 페이지 생성(layout, SignForm 적옹)

---
+++ pages/todo/signin/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignForm from '@/components/todo/sign/SignForm';
+import TodoLayout from '../Todo.Layout';
+
+function SignInPage() {
+  return (
+    <TodoLayout>
+      <SignForm />
+    </TodoLayout>
+  );
+}
+
+export default SignInPage;
---
+++ pages/todo/signup/index.tsx
@@ -0,0 +1,12 @@
+import SignForm from '@/components/todo/sign/SignForm';
+import TodoLayout from '../Todo.Layout';
+
+function SignUpPage() {
+  return (
+    <TodoLayout>
+      <SignForm />
+    </TodoLayout>
+  );
+}
+
+export default SignUpPage;
