5cd2992461d5c27ca466ccc7e6dd0dea8a2dd3d5
thumbthing <ilscc0902@gmail.com>
2023-09-25 18:10:03 +0900

    Feat: 토큰 여부에 따른 리다이렉트 처리 기능 추가

--- pages/todo/index.tsx
+++ pages/todo/index.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import SignForm from '@/components/todo/sign/SignForm';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 import TodoLayout from './Todo.Layout';
 
 export default function TodoPage() {
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const signInUrl = `${pathName}/signin`;
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('ACCESS_TOKEN');
+    if (!accessToken) {
+      router.push(signInUrl);
+    }
+  }, [router, signInUrl]);
+
   return (
     <TodoLayout>
-      <div>
-        <SignForm />
-      </div>
+      <div>todo list</div>
     </TodoLayout>
   );
 }
