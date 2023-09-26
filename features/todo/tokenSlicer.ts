2a9445add0792b4b8ca92ac370d1d5c76cfc8ee7
thumbthing <ilscc0902@gmail.com>
2023-09-27 00:11:31 +0900

    Feat: todoSlicer state 추가, reducer 기능 추가(create, read, update, delelte)

--- features/todo/tokenSlicer.ts
+++ features/todo/tokenSlicer.ts
@@ -1,13 +1,27 @@
-import { TokenType } from '@/types/todo/todo.type';
+import { TodoPropsType, TodoType } from '@/types/todo/todo.type';
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
-const initialState: TokenType = {
+const initialState: TodoPropsType = {
   accessToken: '',
+  todo: {
+    id: 0,
+    todo: '',
+    isCompleted: false,
+    userId: 0,
+  },
+  todoList: [
+    {
+      id: 0,
+      todo: 'Todo를 입력해주세요',
+      isCompleted: false,
+      userId: 0,
+    },
+  ],
 };
 
 export const todoSlice = createSlice({
-  name: 'token',
+  name: 'todo',
   initialState,
   reducers: {
     addToken: (draftState, action: PayloadAction<string>) => {
@@ -16,6 +30,22 @@ export const todoSlice = createSlice({
     },
     deleteToken: (draftState) => {
       draftState.accessToken = '';
+    },
+    getTodoList: (draftState, action: PayloadAction<TodoType[]>) => {
+      const todoList = action.payload;
+      draftState.todoList = todoList;
+    },
+    addTodo: (draftState, action: PayloadAction<TodoType>) => {
+      const newTodo = action.payload;
+      draftState.todoList = [...draftState.todoList, newTodo];
+    },
+    updataTodo: (draftState, action: PayloadAction<TodoType>) => {
+      const newTodo = action.payload;
+      draftState.todoList = draftState.todoList.map((todo) => (todo.id === newTodo.id ? newTodo : todo));
+    },
+    deleteTodo: (draftState, action: PayloadAction<number>) => {
+      const deleteId = action.payload;
+      draftState.todoList = draftState.todoList.filter((todo) => todo.id !== deleteId);
     },
   },
 });
